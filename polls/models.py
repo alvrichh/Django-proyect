5501dc550872d11cab18b3915885f21f66a33711
alvrichh <alvarorodriguezmolina0@gmail.com>
2024-01-29 13:51:45 +0100

    añadidas preguntas por usuario para administracion

--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,8 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 class Question(models.Model):
     CATEGORY_CHOICES = [
@@ -17,15 +19,15 @@ class Question(models.Model):
     updated_at = models.DateTimeField("date updated", default=timezone.now)
     comment = models.TextField(blank=True, null=True)  # Campo opcional de tipo texto
     category = models.CharField(max_length=20, choices=CATEGORY_CHOICES, default='A')
-
+    pollster = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    
     def __str__(self):
         return self.question_text
 
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=self.DAYS_RECENT) <= self.pub_date <= now
-
-
+    
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
